01eede17156b87e9b9986d94d073870b8ed29f61
ShaikMusthak <shaikmusthaq21@gmail.com>
2025-09-18 12:39:08 +0530

    Add files via upload

---
+++ routes/resturantRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const authMiddleware = require("../middlewares/authMiddleware");
+const {
+  createResturantController,
+  getAllResturantController,
+  getResturantByIdController,
+  deleteResturantController,
+} = require("../controllers/resturantController");
+
+const router = express.Router();
+
+//routes
+// CRAETE RESTURANT || POST
+router.post("/create", createResturantController);
+
+// GET ALL RESTURANTS || GET
+router.get("/getAll", getAllResturantController);
+
+// GET RESTURANT BY ID || GET
+router.get("/get/:id", getResturantByIdController);
+
+// DELETE RESTURANT || DELETE
+router.delete("/delete/:id", deleteResturantController);
+
+module.exports = router;
